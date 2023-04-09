63d8f00470ca33519c9147c45c36970f088aa44b
berhan-helloworld <simonadhana@gmail.com>
2023-04-09 04:29:45 -0600

    cleaning unnecessary properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,11 +18,6 @@ export default {
       },
       backgroundImage: {
         mainAccent: ["linear-gradient(#FFC593, #BC7198, #5A77FF)"],
-        facebook: ["linear-gradient(225deg, #63AFDB 0%, #6028F1 100%)"],
-        youtube: ["linear-gradient(#D3205A, #FF5A5A)"],
-        twitter: ["linear-gradient(#65FFEB, #27718A)"],
-        pinterest: ["linear-gradient(#F6C683, #DE3838)"],
-        instagram: ["linear-gradient(#FFC593, #BC7198, #5A77FF)"],
         overlay: [
           "linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0.27%, rgba(0, 0, 0, 0.661222) 100%)",
         ],
