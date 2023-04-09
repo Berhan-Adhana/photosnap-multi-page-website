041461fb4783d0659aab3c9ff25f00dc49d2088d
berhan-helloworld <simonadhana@gmail.com>
2023-04-08 18:56:03 -0600

    text wraping removed

--- src/components/shared/Feature.jsx
+++ src/components/shared/Feature.jsx
@@ -3,7 +3,7 @@ const Feature = ({ feature }) => {
     <article className="flex flex-col text-black items-center">
       <img src={feature.icon} alt={feature.name} className="" />
 
-      <h3 className="mt-12">{feature.name}</h3>
+      <h3 className="mt-12 whitespace-nowrap">{feature.name}</h3>
       <p className="text-black opacity-60 mix-blend-normal text-center mt-4 md:w-[457px]">
         {feature.desc}
       </p>
