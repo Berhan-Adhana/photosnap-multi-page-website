87e0039726123b49b268b6787f50b8dd3d490784
berhan-helloworld <simonadhana@gmail.com>
2023-04-09 19:00:53 -0600

    on hover effect added

--- src/components/pricing/PricePlan.jsx
+++ src/components/pricing/PricePlan.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PricePlan = ({ plan, isBgBlack, isMonthly = false }) => {
   return (
     <article
-      className={`relative pt-[56px] pb-10 flex flex-col items-center md:items-start lg:items-center  px-10 w-[318px] lg:w-[350px]  md:w-full shadow-sm transition-all duration-500 ${
+      className={`relative pt-[56px] hover:shadow-planShadow pb-10 flex flex-col items-center md:items-start lg:items-center  px-10 w-[318px] lg:w-[350px]  md:w-full shadow-sm transition-all duration-500 ${
         isBgBlack
           ? "bg-black before:content-[''] before:absolute before:bg-mainAccent before:left-0 before:h-[4px] before:w-[318px]  md:before:w-full lg:before:w-[350px] before:top-0   "
           : "bg-bgcontainer"
