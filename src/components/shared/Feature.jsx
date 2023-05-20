8da027864ad210e46eb45c244627e397a3606009
berhan-helloworld <simonadhana@gmail.com>
2023-05-20 09:11:35 -0600

    layout enhanced

--- src/components/shared/Feature.jsx
+++ src/components/shared/Feature.jsx
@@ -1,10 +1,14 @@
 const Feature = ({ feature }) => {
   return (
     <article className="flex flex-col text-black items-center">
-      <img src={feature.icon} alt={feature.name} className="" />
+      <img
+        src={feature.icon}
+        alt={feature.name}
+        className="w-[80px] h-[64px]"
+      />
 
       <h3 className="mt-12 whitespace-nowrap">{feature.name}</h3>
-      <p className="text-black opacity-60 mix-blend-normal text-center mt-4 md:w-[457px]">
+      <p className="text-black opacity-60 mix-blend-normal text-center mt-4">
         {feature.desc}
       </p>
     </article>
